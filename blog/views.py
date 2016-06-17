c6e3ee4af1c44e90c0f705ef8f6b202f0b314bce
berlin <fanbailin@126.com>
2016-06-17 16:26:45 +0800

    使用模板方式返回一个http请求

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,16 @@
 #coding:utf-8
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+import datetime
+
+'''
+def blog_index(request):
+    return HttpResponse(u"Hello Django")
+'''
 
 
 def blog_index(request):
-    return HttpResponse(u"Hello Django")
+    now = datetime.datetime.now()
+    otherStyleTime = now.strftime("%Y-%m-%d %H:%M:%S")
+    text = "Welcom to learn django"
+    return render_to_response('blog/index.html',locals())
