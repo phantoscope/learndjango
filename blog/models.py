b6fd573eb6aefb5754634b31cd64e23cbc2bed72
berlin <fanbailin@126.com>
2016-06-17 17:50:37 +0800

    定义博客所需要的models

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,49 @@
-from __future__ import unicode_literals
-
+#coding:utf-8
 from django.db import models
 
-# Create your models here.
+
+# tag（标签）
+class Tag(models.Model):
+    name = models.CharField(max_length=30, verbose_name='标签名称')
+
+    class Meta:
+        verbose_name = '标签'
+        verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return self.name
+
+# 分类
+class Category(models.Model):
+    name = models.CharField(max_length=30, verbose_name='分类名称')
+    index = models.IntegerField(default=999,verbose_name='分类的排序')
+
+    class Meta:
+        verbose_name = '分类'
+        verbose_name_plural = verbose_name
+        ordering = ['index', 'id']
+
+    def __unicode__(self):
+        return self.name
+
+# 文章
+class Article(models.Model):
+    title = models.CharField(max_length=50, verbose_name='文章标题')
+    desc = models.CharField(max_length=50, blank=True,null=True,verbose_name='文章描述')
+    content = models.TextField(verbose_name='文章内容')
+    view_count = models.IntegerField(default=0, verbose_name='查看次数')
+    love_count = models.IntegerField(default=0, verbose_name='点赞次数')
+    is_recommend = models.BooleanField(default=False, verbose_name='是否推荐')
+    date_publish = models.DateTimeField(auto_now_add=True, verbose_name='发布时间')
+    date_update = models.DateTimeField(auto_now_add=True, verbose_name='更新时间')
+    category = models.ForeignKey(Category, blank=True, null=True, verbose_name='分类')
+    tag = models.ManyToManyField(Tag, verbose_name='标签')
+
+
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = verbose_name
+        ordering = ['-date_publish']
+
+    def __unicode__(self):
+        return self.title
