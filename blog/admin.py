2124ae2c501db5f0c73ed9dc9517a4c06f3c5503
berlin <fanbailin@126.com>
2016-06-17 18:00:39 +0800

    将app注册到django admin里,然后根据models生成blog表

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
+#coding:utf-8
 from django.contrib import admin
+from blog.models import *
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'desc', 'view_count','is_recommend','date_publish')
+    list_display_links = ('title', 'desc', )
+    list_editable = ('view_count',)
+
+
 
 # Register your models here.
+admin.site.register(Tag)
+admin.site.register(Category)
+admin.site.register(Article,ArticleAdmin)
